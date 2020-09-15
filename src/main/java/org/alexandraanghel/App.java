a8f1d419a77bac21c89af8fdb3767e96d949ef11
alexandraioanaanghel <alexandranghel2017@gmail.com>
2020-09-15 16:34:37 +0300

    Initial commit (project structure)

---
+++ src/main/java/org/alexandraanghel/App.java
@@ -0,0 +1,13 @@
+package org.alexandraanghel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
