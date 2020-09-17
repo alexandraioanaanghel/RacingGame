0eac958053c4bc6a5e3af955bacb81ffed318474
alexandraioanaanghel <alexandranghel2017@gmail.com>
2020-09-17 11:00:56 +0300

    studied variables

--- src/main/java/org/alexandraanghel/Car.java
+++ src/main/java/org/alexandraanghel/Car.java
@@ -10,4 +10,12 @@ public class Car {
     int doorCount;
     boolean damaged;
     String color;
+
+    //Primitive Data Types:
+    // byte (cand lucram cu fis binare), short, int, long(pt intreg, pt milisecunde, pt id din baza de date), float, double, boolean, char
+
+    //Wrapper(impachetare) class
+    // Double testDouble;
+    // Integer testInt;
+    // Character testChar;
 }
