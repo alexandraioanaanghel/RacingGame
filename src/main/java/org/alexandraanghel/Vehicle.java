6f51133af0c067e0e7e3569cccd47e88e9f12060
alexandraioanaanghel <alexandranghel2017@gmail.com>
2020-09-22 13:18:28 +0300

    declaring methods

--- src/main/java/org/alexandraanghel/Vehicle.java
+++ src/main/java/org/alexandraanghel/Vehicle.java
@@ -2,6 +2,7 @@ package org.alexandraanghel;
 
 public class Vehicle {
 
+    // instance variables
     String name;
     double fuelLevel;
     double mileage; //consum
@@ -9,4 +10,23 @@ public class Vehicle {
     double maxSpeed;
     boolean damaged;
     String color;
+
+    public double accelerate(double speed, double durationInHours)
+    {
+        System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + " h");
+
+        //local variable
+        double distance = speed * durationInHours;
+
+        totalTraveledDistance += distance;
+        System.out.println("Total traveled distance for vehicle " + name + ": " + totalTraveledDistance);
+
+        double usedFuel = distance * mileage /100;
+
+        fuelLevel -= usedFuel;
+        System.out.println("Remaining fuel for vehicle " + name + ": " + fuelLevel);
+
+
+        return distance;
+    }
 }
